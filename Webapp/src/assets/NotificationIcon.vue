37f860328ad02814000405456fb441647a2c3146
RuMaroNS <sdulimos@gmail.com>
2025-07-26 17:34:40 +0700

    Add files via upload

---
+++ Webapp/src/assets/NotificationIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 8.75V14.75"
+      stroke="#B1C1D2"
+      stroke-width="1.5"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+    />
+    <path d="M9 11.75H15" stroke="#B1C1D2" stroke-linecap="round" />
+    <path
+      d="M12.02 2.75C8.34002 2.75 5.36002 5.73 5.36002 9.41V11.51C5.36002 12.19 5.08002 13.21 4.73002 13.79L3.46002 15.91C2.68002 17.22 3.22002 18.68 4.66002 19.16C9.44002 20.75 14.61 20.75 19.39 19.16C20.74 18.71 21.32 17.13 20.59 15.91L19.32 13.79C18.97 13.21 18.69 12.18 18.69 11.51V9.41C18.68 5.75 15.68 2.75 12.02 2.75Z"
+      stroke="#B1C1D2"
+      stroke-width="1.5"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+    />
+    <path
+      d="M15.33 19.57C15.33 21.4 13.83 22.9 12 22.9C11.09 22.9 10.25 22.52 9.65004 21.92C9.05004 21.32 8.67004 20.48 8.67004 19.57"
+      stroke="#B1C1D2"
+      stroke-width="1.5"
+      stroke-miterlimit="10"
+    />
+  </svg>
+</template>
